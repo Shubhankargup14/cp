b111930474f60dd1d0a3a4c60e720759410c49d2
Shubhankar Gupta <96997071+Shubhankargup14@users.noreply.github.com>
2023-01-21 02:37:53 +0530

    Add files via upload

---
+++ 974. Subarray Sums Divisible by K.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+    
+        int [] remainderArray = new int[k];
+        remainderArray[0] = 1;
+        int sum = 0, count = 0;
+
+        for(int arr : nums){
+            sum = (sum + arr) % k;
+
+            if(sum < 0) sum += k;
+            count += remainderArray[sum];
+            remainderArray[sum]++;
+            
+           }
+        
+        return count;
+    }
+}
