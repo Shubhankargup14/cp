6c9060b44c37f35c592f29f9929af5eb71c308b4
Shubhankar Gupta <96997071+Shubhankargup14@users.noreply.github.com>
2023-02-27 22:47:10 +0530

    Update Print_all_subsequence_which_sums_to_k.java

--- Print_all_subsequence_which_sums_to_k.java
+++ Print_all_subsequence_which_sums_to_k.java
@@ -2,10 +2,12 @@ import java.util.*;
 class ksum {
     public static void main(String[] args) {
         int arr[]={1,2,1};
+        int n=arr.length;
+        int i=0,s=0;
         List<Integer>list=new ArrayList<>();
-        s(0,3,0,list,arr,2);
+        s(i,n,s,list,arr,2);
     }
-    static void s(int i,int n,int s,List<Integer>list,int arr[],int sum){
+    static void subs(int i,int n,int s,List<Integer>list,int arr[],int sum){
         
         if(i==n){
         if(s==sum){
@@ -17,10 +19,10 @@ class ksum {
        
         list.add(arr[i]);
         s+=arr[i];
-        s(i+1,n,s,list,arr,sum);
+        subs(i+1,n,s,list,arr,sum);
         list.remove(list.size()-1);
         s-=arr[i];
-        s(i+1,n,s,list,arr,sum);
+        subs(i+1,n,s,list,arr,sum);
         
        
     }
